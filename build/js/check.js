d9e17e71775cfee3720e7018bfdd70635446a780
Funky-Juice <isa86box@gmail.com>
2016-03-27 20:44:36 +0300

    внес правки в скрипт

--- build/js/check.js
+++ build/js/check.js
@@ -1,15 +1,24 @@
-function getMessage(a:*, b:*=):string {
-	if (a == boolean) {
-		if (a == true) {
-			return ('Переданное GIF-изображение анимировано и содержит' + [b] + 'кадров');
-		  } else if (a === false){
-			  return "Переданное GIF-изображение не анимировано";
-		    };
-  } else if (a == number) {
-  	return ('Переданное SVG-изображение содержит' + [a] + 'объектов и' [b * 4] 'атрибутов');
-    } else if (a == []) {
-    	  return ('Количество красных точек во всех строчках изображения:' + [sum]);
-      } else if (a == [] && b == []) {
-      	  return ('Общая площадь артефактов сжатия:' + [square] + 'пикселей');
+function getMessage(a, b) {
+
+  if (typeof(a) == boolean) {
+    if (a == true) {
+    return 'Переданное GIF-изображение анимировано и содержит' + b + 'кадров';
+      } else {
+      return 'Переданное GIF-изображение не анимировано';
+    };
+  } else if (typeof(a) == number) {
+      return 'Переданное SVG-изображение содержит' + a + 'объектов и' + (b * 4) + 'атрибутов';
+    } else if (typeof(a) == object && typeof(b) == object) {
+        var square = 0;
+        for (i = 0; i < a.length; i++) {
+          square += a[i] * b[i];
+          }
+      return 'Общая площадь артефактов сжатия:' + square + 'пикселей';
+      } else if (typeof(a) == object) {
+          var sum = 0;
+          for (i = 0; i < a.length; i++) {
+            sum = a[i];
+          }
+          return 'Количество красных точек во всех строчках изображения:' + sum;
         };
 };
