f70412330d36cbe2f7c627bc27f81dfb0c720625
Funky-Juice <isa86box@gmail.com>
2016-04-18 11:33:48 +0300

    написал основной код для задания module4-task1

---
+++ src/pictures.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function() {
+
+  var filtersForm = document.querySelector('.filters');
+  filtersForm.classList.add('hidden');
+
+  var picturesContainer = document.querySelector('.pictures');
+
+  var templateElement = document.querySelector('#picture-template');
+
+  if ('content' in templateElement) {
+    var elementToClone = templateElement.content.querySelector('.picture');
+  } else {
+    elementToClone = templateElement.querySelector('.picture');
+  }
+
+  /** @constant {number} */
+  var IMAGE_LOAD_TIMEOUT = 10000;
+
+  /**
+   * @param {Object} data
+   * @param {HTMLElement} container
+   * @return {HTMLElement}
+   */
+  var getPictureElement = function(data, container) {
+    var pictureElement = elementToClone.cloneNode(true);
+    container.appendChild(pictureElement);
+
+    var contentImage = new Image();
+    var contentLoadTimeout;
+
+    contentImage.onload = function() {
+      clearTimeout(contentLoadTimeout);
+      pictureElement.style.contentImage = 'url(\'' + contentImage.src + '\')';
+    };
+
+    contentImage.onerror = function() {
+      pictureElement.classList.add('picture-load-failure');
+    };
+
+    contentImage.src = data.url;
+
+    contentLoadTimeout = setTimeout(function() {
+      contentImage.src = '';
+      pictureElement.classList.add('picture-load-failure');
+    }, IMAGE_LOAD_TIMEOUT);
+
+    return pictureElement;
+  }
+
+  pictures.forEach(function(picture) {
+    getPictureElement(picture, picturesContainer);
+  });
+
+  //filtersForm.classList.remove('hidden');
+
+})();
