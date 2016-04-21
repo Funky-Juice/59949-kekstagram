b4f4647d135d63b927135d5d01ac79fca7d99be1
Funky-Juice <isa86box@gmail.com>
2016-04-21 16:20:41 +0300

    сделал задание module5-task1

--- src/pictures.js
+++ src/pictures.js
@@ -19,6 +19,22 @@
   /** @constant {number} */
   var IMAGE_LOAD_TIMEOUT = 10000;
 
+  /** @constant {string} */
+  var PICTURES_LOAD_URL = '//o0.github.io/assets/json/pictures.json';
+
+  /** @type {Array.<Object>} */
+  var pictures = [];
+
+  /** @enum {number} */
+  var Filter = {
+    'POPULAR': 'filter-popular',
+    'NEW': 'filter-new',
+    'DISCUSSED': 'filter-discussed'
+  };
+
+  /** @constant {Filter} */
+  var DEFAULT_FILTER = Filter.POPULAR;
+
   /**
    * @param {Object} data
    * @param {HTMLElement} container
@@ -27,18 +43,20 @@
   var getPictureElement = function(data, container) {
     var pictureElement = elementToClone.cloneNode(true);
 
-    container.appendChild(pictureElement);
-
     var contentImage = new Image();
     var contentLoadTimeout;
 
+    /** @param {ProgressEvent} evt */
     contentImage.onload = function() {
       clearTimeout(contentLoadTimeout);
+
       pictureElement.querySelector('img').src = data.url;
     };
 
     contentImage.onerror = function() {
       pictureElement.classList.add('picture-load-failure');
+
+      picturesContainer.classList.add('pictures-failure');
     };
 
     contentImage.src = data.url;
@@ -46,14 +64,98 @@
     contentLoadTimeout = setTimeout(function() {
       contentImage.src = '';
       pictureElement.classList.add('picture-load-failure');
+      picturesContainer.classList.add('pictures-failure');
     }, IMAGE_LOAD_TIMEOUT);
 
+    container.appendChild(pictureElement);
     return pictureElement;
   };
 
-  window.pictures.forEach(function(picture) {
-    getPictureElement(picture, picturesContainer);
+  picturesContainer.classList.add('pictures-loading');
+
+  /** @param {Array.<Object>} pictures */
+  var renderPictures = function(pictures) {
+    picturesContainer.innerHTML = '';
+
+    pictures.forEach(function(picture) {
+      getPictureElement(picture, picturesContainer);
+    });
+  };
+
+  /**
+   * @param {Array.<Object>} pictures
+   * @param {Filter} filter
+   */
+  var getFilteredPictures = function(pictures, filter) {
+    var picturesToFilter = pictures.slice(0);
+
+    switch (filter) {
+      case Filter.POPULAR:
+        picturesToFilter.sort(function(a, b) {
+          return b.likes - a.likes;
+        });
+        break;
+
+      case Filter.NEW:
+        picturesToFilter.sort(function(a, b) {
+          return Date.parse(b.date) - Date.parse(a.date);
+        });
+        break;
+
+      case Filter.DISCUSSED:
+        picturesToFilter.sort(function(a, b) {
+          return b.comments - a.comments;
+        });
+        break;
+    }
+
+    return picturesToFilter;
+  };
+
+  /** @param {Filter} filter */
+  var setFilterEnabled = function(filter) {
+    var filteredPictures = getFilteredPictures(pictures, filter);
+    renderPictures(filteredPictures);
+
+    //var activeFilter = filtersForm.querySelector(checked);
+    //if (activeFilter) {
+    //  activeFilter.removeAttribute('checked');
+    //}
+    //var filterToActivate = document.getElementById(filter);
+    //filterToActivate.setAttribute('checked', 'checked');
+  };
+
+  /** @param {boolean} enabled */
+  var setFiltersEnabled = function(enabled) {
+    var filters = filtersForm.querySelectorAll('.filters-radio');
+    for (var i = 0; i < filters.length; i++) {
+      filters[i].onclick = enabled ? function(evt) {
+        setFilterEnabled(this.id);
+      } : null;
+    }
+  };
+
+  /** @param {function(Array.<Object>)} callback */
+  var getPictures = function (callback) {
+    var xhr = new XMLHttpRequest();
+
+    /** @param {ProgressEvent} */
+    xhr.onload = function (evt) {
+      var loadedData = JSON.parse(evt.target.response);
+      callback(loadedData);
+    };
+
+    xhr.open('GET', PICTURES_LOAD_URL, true);
+    xhr.send();
+  };
+
+  getPictures(function(loadedPictures) {
+    pictures = loadedPictures;
+    setFiltersEnabled(true);
+    setFilterEnabled(DEFAULT_FILTER);
   });
+
+  picturesContainer.classList.remove('pictures-loading');
 
   filtersForm.classList.remove('hidden');
 })();
