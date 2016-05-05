6fe936ca85507f2b62d8a19a072cc21ab714b55e
Funky-Juice <isa86box@gmail.com>
2016-05-06 00:39:05 +0300

    переписал код проекта через CommonJS

---
+++ src/forms/filter-form.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var browserCookies = require('browser-cookies');
+
+var resizeModule = require('./resize-form');
+
+resizeModule.resizeForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+  if (resizeModule.resizeFormIsValid()) {
+    filterImage.src = resizeModule.currentResizer.exportImageFromResizer();
+
+    resizeModule.resizeForm.classList.add('invisible');
+    filterForm.classList.remove('invisible');
+  }
+});
+
+/**
+ * Форма добавления фильтра.
+ * @type {HTMLFormElement}
+ */
+var filterForm = document.forms['upload-filter'];
+
+/**
+ * @type {Object.<string, string>}
+ */
+var filterMap = {
+  'none': 'filter-none',
+  'chrome': 'filter-chrome',
+  'sepia': 'filter-sepia'
+};
+
+/**
+ * @type {HTMLImageElement}
+ */
+var filterImage = filterForm.querySelector('.filter-image-preview');
+
+/**
+ * Сброс формы фильтра. Показывает форму кадрирования.
+ * @param {Event} evt
+ */
+filterForm.addEventListener('reset', function(evt) {
+  evt.preventDefault();
+
+  filterForm.classList.add('invisible');
+  resizeModule.resizeForm.classList.remove('invisible');
+});
+
+/** Расчет дней до истечения cookie. */
+function daysToExpire() {
+  var today = new Date();
+  var birthYear = today.getFullYear();
+
+  if(+today > +new Date(birthYear, 9, 13)) {
+    var dateToExpire = +today + (+today - (+new Date(birthYear, 9, 13)));
+  } else {
+    dateToExpire = +today + (+today - (+new Date(birthYear - 1, 9, 13)));
+  }
+
+  return new Date(dateToExpire).toUTCString();
+}
+
+var formattedDateToExpire = daysToExpire();
+
+/**
+ * Функция записи последнего фильтра в cookie.
+ */
+function setLastFilterToCookie() {
+  var chekedFilterId = function () {
+    var inputField = filterForm.elements['upload-filter'];
+
+    for (var i = 0; i < inputField.length; i++) {
+      if (inputField[i].checked) {
+        return inputField[i].id;
+      }
+    }
+    return '';
+  };
+
+  browserCookies.set('selectedFilter', chekedFilterId(), {
+    expires: formattedDateToExpire
+  });
+}
+
+/**
+ * Обработчик изменения фильтра. Добавляет класс из filterMap соответствующий
+ * выбранному значению в форме.
+ */
+filterForm.addEventListener('change', function() {
+
+  var selectedFilter = [].filter.call(filterForm['upload-filter'], function (item) {
+    return item.checked;
+  })[0].value;
+
+  // Класс перезаписывается, а не обновляется через classList потому что нужно
+  // убрать предыдущий примененный класс. Для этого нужно или запоминать его
+  // состояние или просто перезаписывать.
+  filterImage.className = 'filter-image-preview ' + filterMap[selectedFilter];
+
+  setLastFilterToCookie();
+});
+
+module.exports = {
+  filterForm: filterForm,
+  setLastFilterToCookie: setLastFilterToCookie
+};
---
+++ src/forms/resize-form.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var Resizer = require('../resizer');
+
+var utilities = require('../utilities');
+
+var uploadModule = require('./upload-form');
+
+/**
+ * Форма кадрирования изображения.
+ * @type {HTMLFormElement}
+ */
+var resizeForm = document.forms['upload-resize'];
+var resizeX = document.querySelector('#resize-x');
+var resizeY = document.querySelector('#resize-y');
+var resizeSize = document.querySelector('#resize-size');
+var resizeFwd = document.querySelector('#resize-fwd');
+
+resizeX.value = 0;
+resizeY.value = 0;
+resizeSize.value = 200;
+
+/**
+ * Объект, который занимается кадрированием изображения.
+ * @type {Resizer}
+ */
+var currentResizer;
+
+/**
+ * Удаляет текущий объект {@link Resizer}, чтобы создать новый с другим
+ * изображением.
+ */
+function cleanupResizer() {
+  if (currentResizer) {
+    currentResizer.remove();
+    currentResizer = null;
+  }
+}
+cleanupResizer();
+
+/**
+ * Обработчик изменения изображения в форме загрузки. Если загруженный
+ * файл является изображением, считывается исходник картинки, создается
+ * Resizer с загруженной картинкой, добавляется в форму кадрирования
+ * и показывается форма кадрирования.
+ * @param {Event} evt
+ */
+uploadModule.uploadForm.addEventListener('change', function(evt) {
+  var element = evt.target;
+  if (element.id === 'upload-file') {
+    // Проверка типа загружаемого файла, тип должен быть изображением
+    // одного из форматов: JPEG, PNG, GIF или SVG.
+    if (utilities.isImgFile(element.files[0].type)) {
+      var fileReader = new FileReader();
+
+      uploadModule.showUploadingMsg();
+
+      fileReader.onload = function() {
+        cleanupResizer();
+
+        currentResizer = new Resizer(fileReader.result);
+        currentResizer.setElement(resizeForm);
+
+        uploadModule.uploadForm.classList.add('invisible');
+        resizeForm.classList.remove('invisible');
+        uploadModule.hideMessage();
+      };
+
+      fileReader.readAsDataURL(element.files[0]);
+    } else {
+      // Показ сообщения об ошибке, если загружаемый файл, не является
+      // поддерживаемым изображением.
+      uploadModule.showErrorMsg();
+    }
+  }
+});
+
+/**
+ * Проверяет, валидны ли данные, в форме кадрирования.
+ * @return {boolean}
+ */
+function resizeFormIsValid() {
+  if ((+resizeX.value + +resizeSize.value < currentResizer._image.naturalWidth)
+    && (+resizeY.value + +resizeSize.value < currentResizer._image.naturalHeight)
+    && (resizeX.value >= 0 && resizeY.value >= 0)) {
+    resizeFwd.removeAttribute('disabled');
+    return true;
+  } else {
+    resizeFwd.setAttribute('disabled', 'disabled');
+    return false;
+  }
+}
+
+/**
+ * Функция возвращает адрес обрезанной картинки из ресайзера
+ */
+function exportImageFromResizer() {
+  return currentResizer.exportImage().src;
+}
+
+/**
+ * Перерисовка currentResiser по изменению значений полей формы.
+ */
+resizeX.addEventListener('input', resizeFormIsValid);
+
+resizeY.addEventListener('input', resizeFormIsValid);
+
+resizeSize.addEventListener('input', resizeFormIsValid);
+
+resizeForm.addEventListener('input', function() {
+  currentResizer.setConstraint(parseInt(resizeX.value, 10), parseInt(resizeY.value, 10), parseInt(resizeSize.value, 10));
+});
+
+/** Изменение значений полей по перетаскиванию изображения мышью. */
+window.addEventListener('resizerchange', function() {
+  var squareObj = currentResizer.getConstraint();
+  resizeSize.value = squareObj.side;
+  resizeX.value = squareObj.x;
+  resizeY.value = squareObj.y;
+});
+
+module.exports = {
+  resizeForm: resizeForm,
+  cleanupResizer: cleanupResizer,
+  resizeFormIsValid: resizeFormIsValid,
+  exportImageFromResizer: exportImageFromResizer
+};
